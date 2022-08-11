d528676881dd87aba5225da5ce1dccce426fdede
taielfj <taielfernandezjara@gmail.com>
2022-08-11 10:50:12 -0300

    subiendo calculadora

---
+++ app.js
@@ -0,0 +1,11 @@
+const suma = require('./sumar');
+const resta = reqyuire('./restar');
+const multiplicar = require('./multiplicar');
+const dividir = require('./dividir');
+
+
+
+console.log(suma(1, 5) + " resultado de la suma")
+console.log(resta(10, 10) + " resultado de la resta")
+console.log(multiplicar(2,5) + " resultado de la multiplicacion")
+console.log(dividir(2,5) + " resultado de la division")
---
+++ dividir.js
@@ -0,0 +1,7 @@
+module.exports = function(a,b){
+    if(a === 0 || b === 0){
+        return "no se puede dividir por CERO"
+    }else{
+        return a / b;
+    }
+}
